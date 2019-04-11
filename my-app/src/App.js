c6f294ad84ad64d48f2d763d9d707bd4116b7128
Alex Zhorov <3767082@mail.ru>
2019-04-11 22:31:04 +0300

    123

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Info from "./components/info";
+import AppRouter from "./components/Routing"
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Info />
+            </div>
+        );
+    }
+}
+export default App;
