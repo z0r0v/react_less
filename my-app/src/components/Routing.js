aa28b7f2e3322ebc570a1198840bdd5525cd1d25
Alex Zhorov <3767082@mail.ru>
2019-04-12 11:25:51 +0300

    1234

--- my-app/src/components/Routing.js
+++ my-app/src/components/Routing.js
@@ -4,27 +4,39 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function login() {
     return (
     <div>
-        <span>Login </span><input type="text" placeholder="Enter you email or login"></input><br/>
-        <span>Password </span><input type="password" placeholder="Enter you Password"></input><br/>
-        <button style={{backgroundColor:"#44014c", color:"#fff", width:"150px"}} type="submit">Login you account</button>
+        <span>Login </span>
+        <input style={{margin:"5px 0"}} type="text" placeholder="Enter you email or login"></input>
+        <br/>
+        <span>Password </span>
+        <input style={{margin:"5px 0"}} type="password" placeholder="Enter you Password"></input>
+        <br/>
+        <button style={{backgroundColor:"#44014c", color:"#fff", width:"150px", height: "30px", margin:"15px 0"}} type="submit">Login you account</button>
     </div>);
 }
 
 function register() {
     return(  <div>
-        <span>Enter you email </span><input type="text" placeholder="Enter you email"></input><br/>
-        <span>Create password </span><input type="password" placeholder="Enter you Password"></input><br/>
-        <span>Confirm password </span><input type="password" placeholder="Confirm you Password"></input><br/>
-        <button style={{backgroundColor:"#44014c", color:"#fff", width:"150px"}} type="submit">Create account</button>
+        <span>Enter you email </span>
+        <input style={{margin:"5px 0"}} type="text" placeholder="Enter you email"></input>
+        <br/>
+        <span>Create password </span>
+        <input style={{margin:"5px 0"}} type="password" placeholder="Enter you Password"></input>
+        <br/>
+        <span>Confirm password </span>
+        <input style={{margin:"5px 0"}} type="password" placeholder="Confirm you Password"></input>
+        <br/>
+        <button style={{backgroundColor:"#44014c", color:"#fff", width:"150px", height: "30px",  margin:"15px 0"}} type="submit">Create account</button>
     </div>);
 }
 
 function profile() {
     return (
         <div>
-        <span>Сhange your content </span><br/>
-        <input type="text" placeholder="Enter your content here"></input><br/>
-            <button style={{backgroundColor:"#44014c", color:"#fff", width:"150px"}} type="submit">Apply</button>
+        <span>Сhange your content </span>
+            <br/>
+        <input style={{margin:"10px 0"}} type="text" placeholder="Enter your content here" style={{width:"350px", height: "150px",}}></input>
+            <br/>
+        <button style={{backgroundColor:"#44014c", color:"#fff", width:"150px", height: "30px",  margin:"15px 0"}} type="submit">Apply</button>
         </div>);
 }
 
